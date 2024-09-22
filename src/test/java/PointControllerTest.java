b54e7ec2f831386a3599efa4c0b65dd94a5ebe12
jewon shin <jw8421ss@nate.com>
2024-09-23 02:13:25 +0900

    Add project files in submodule

---
+++ src/test/java/PointControllerTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+
+public class PointControllerTest {
+
+	// 특정 유저의 포인트를 조회하는 기능 테스트
+	@Test
+	void testPoint() {
+
+		// given
+
+		// when
+
+		// then
+
+	}
+
+	// 특정 유저의 포인트 충전/이용 내역을 조회하는 기능 테스트
+	@Test
+	void testHistory() {
+
+		// given
+
+		// when
+
+		// then
+
+	}
+
+	// 특정 유저의 포인트를 충전하는 기능 테스트
+	@Test
+	void testCharge() {
+
+		// given
+
+		// when
+
+		// then
+
+	}
+
+	// 특정 유저의 포인트를 사용하는 기능 테스트
+	@Test
+	void testUse() {
+
+		// given
+
+		// when
+
+		// then
+
+	}
+}
